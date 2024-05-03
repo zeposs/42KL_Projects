a7d3c8754f3b6a3c720970a584b988f43786f00c
Zep Ernest <zernest@u82z10s02.42kl.edu.my>
2024-05-03 13:26:29 +0800

    added c02

--- c02/ex02/ft_str_is_alpha.c
+++ c02/ex02/ft_str_is_alpha.c
@@ -1,41 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zernest <zernest@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/02 13:32:40 by zernest           #+#    #+#             */
-/*   Updated: 2024/05/02 13:32:47 by zernest          ###   ########.fr       */
+/*   Created: 2024/05/03 13:05:37 by zernest           #+#    #+#             */
+/*   Updated: 2024/05/03 13:05:38 by zernest          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-char	*ft_strncpy(char *dest, char *src, unsigned int n)
+int	ft_str_is_alpha(char *str)
 {
-	unsigned int	letter;
-
-	letter = 0;
-	while ((letter < n) && (src[letter] != '\0'))
+	if (*str == '\0')
 	{
-		dest[letter] = src[letter];
-		letter++;
+		return (1);
 	}
-	dest[letter] = '\0';
-	return (dest);
+	else
+	{
+		while (*str != '\0')
+		{
+			if (!((*str >= 'a' && *str <= 'z') || (*str >= 'A' && *str <= 'Z')))
+			{
+				return (0);
+			}
+			str++;
+		}
+		return (1);
+	}
 }
 
 /* int	main(void)
 {
-	char	dup[500];
-	int		new;
+	int	a;
 
-	ft_strncpy(dup, "Hello", 3);
-	new = 0;
-	while (dup[new] != '\0')
-	{
-		write(1, &dup[new], 1);
-		new ++;
-	}
-} 
+	a = ft_str_is_alpha("hello") + '0';
+	write(1, &a, 1);
+} */
