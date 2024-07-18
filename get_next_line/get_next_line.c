d9850edad830dcfb89d23657661cd31c819f43f8
Zep Ernest <zernest@u82z10s04.42kl.edu.my>
2024-07-18 17:19:20 +0800

    ALMOST DONE

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,26 +6,123 @@
 /*   By: zernest <zernest@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 16:20:50 by zernest           #+#    #+#             */
-/*   Updated: 2024/07/12 17:44:17 by zernest          ###   ########.fr       */
+/*   Updated: 2024/07/18 17:04:12 by zernest          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*ft_readfile(int fd, char *stash)
+{
+	char	*buffer;
+	int		rdbyte;
+
+	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	rdbyte = 1;
+	while (rdbyte > 0)
+	{
+		rdbyte = read(fd, buffer, BUFFER_SIZE);
+		if (rdbyte == -1)
+		{
+			free(stash);
+			free(buffer);
+			return (NULL);
+		}
+		buffer[rdbyte] = '\0';
+		stash = ft_strjoin_free(stash, buffer);
+		if (ft_strchr(buffer, '\n'))
+			break ;
+	}
+	free (buffer);
+	return (stash);
+}
+
+char	*get_line(char *stash)
+{
+	char	*line;
+	int		len;
+
+	if (!stash || stash[0] == '\0')
+		return (NULL);
+	len = 0;
+	while (stash[len] != '\n' && stash[len] != '\0')
+		len++;
+	if (stash[len] == '\n')
+		len++;
+	line = ft_substr(stash, 0, len);
+	return (line);
+}
+
+char	*process_stash(char *stash)
+{
+	char	*new_stash;
+	int		i;
+
+	i = 0;
+	if (!stash || stash[i] == '\0')
+	{
+		free(stash);
+		return (NULL);
+	}
+	while (stash[i] != '\0' || stash[i] != '\n')
+		i++;
+	if (stash[i] == '\0')
+	{
+		free(stash);
+		return (NULL);
+	}
+	else if (stash[i] == '\n')
+		i++;
+	new_stash = ft_substr(stash, i, ft_strlen(stash));
+	free(stash);
+	return (new_stash);
+}
+
 char	*get_next_line(int fd)
 {
 	char		*line;
-	static char	*leftover;
+	static char	*stash;
 
 	if (fd < 0 || BUFFER_SIZE < 1)
 		return (0);
-	leftover = add_to_leftover(fd, leftover);
-
+	if (!stash)
+		stash = ft_strdup("");
+	stash = ft_readfile(fd, stash);
+	line = get_line(stash);
+	stash = process_stash(stash);
+	return (line);
 }
 
-int	main(void)
+#include <fcntl.h>
+#include <stdio.h>
+/* int	main(void)
 {
-	char	value[50];
-
-	value[50] = get_next_line ("text.txt")
-}
+	char	*line;
+	int		i;
+	int		fd1;
+	// int		fd2;
+	// int		fd3;
+	fd1 = open("/Users/zernest/code/get_next_line/tests/test.txt", O_RDONLY);
+	// fd2 = open("tests/test2.txt", O_RDONLY);
+	// fd3 = open("tests/test3.txt", O_RDONLY);
+	i = 1;
+	while (i < 7)
+	{
+		line = get_next_line(fd1);
+		printf("line [%02d]: %s", i, line);
+		free(line);
+		// line = get_next_line(fd2);
+		// printf("line [%02d]: %s", i, line);
+		// free(line);
+		// line = get_next_line(fd3);
+		// printf("line [%02d]: %s", i, line);
+		// free(line);
+		i++;
+	}
+	close(fd1);
+	// close(fd2);
+	// close(fd3);
+	return (0);
+} */
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: zernest <zernest@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 16:21:52 by zernest           #+#    #+#             */
-/*   Updated: 2024/07/12 17:34:39 by zernest          ###   ########.fr       */
+/*   Updated: 2024/07/18 17:00:07 by zernest          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,16 @@
 #  define BUFFER_SIZE 500
 # endif
 
+char	*ft_readfile(int fd, char *stash);
+char	*get_line(char *stash);
+char	*process_stash(char *stash);
 char	*get_next_line(int fd);
 
 // utils
-int	ft_strlen(char const *str);
+int		ft_strlen(char const *str);
+char	*ft_strjoin_free(char *s1, char *s2);
+int		ft_strchr(char *str, int c);
+char	*ft_strdup(const char *s1);
+char	*ft_substr(char const *s, int start, int len);
 
 #endif
