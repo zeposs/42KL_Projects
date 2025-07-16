79895ba53cd0950f7adff709d1024fef7e8ee529
Zep Ernest <zernest@u90z05s03.42kl.edu.my>
2025-07-16 22:09:46 +0800

    started cpp01

---
+++ cpp01/ex00/Zombie.hpp
@@ -0,0 +1,16 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
+#include <cstdio>
+
+class Zombie
+{
+    private:
+        std::string name;
+    public:
+        void announce( void );
+
+}
+
+#endif
