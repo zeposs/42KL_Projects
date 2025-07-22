4c2976ab327eed21cab358087050b891bdae44d8
zepos <zepernest2007@gmail.com>
2025-07-23 06:56:51 +0800

    ex02 done

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+
+int main()
+{
+	std::string var = "HI THIS IS BRAIN";
+	std::string *stringPTR = &var;
+	std:: string &stringREF = var;
+
+	std::cout << "The memory address of the string variable: " << &var << std::endl;
+	std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "The value of the string variable: " << var << std::endl;
+	std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
+
+}
