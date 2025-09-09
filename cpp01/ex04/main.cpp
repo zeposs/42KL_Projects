ca27f3b7aa678fe0dc7a9abca688eeb6fd9fe708
Zep Ernest <zernest@u91z06s01.42kl.edu.my>
2025-09-09 16:48:35 +0800

    bug fix on NULL input

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -11,9 +11,7 @@ std::string replace_line(std::string line, const std::string s1, const std::stri
 	while (pos != std::string::npos)
 	{
 		std::string before = line.substr(0, pos);
-		std::cout << before << std::endl;
 		std::string after = line.substr(pos + s1.length());
-		std::cout << after << std::endl;
 		line = before + s2 + after;
 		pos = line.find(s1, pos + s2.length());
 	}
@@ -32,10 +30,24 @@ int main(int ac, char **av)
 	std::string filename = av[1];
 	std::string s1 = av[2];
 	std::string s2 = av[3];
-
+	if (s1.empty())
+	{
+		std::cerr << "Error: s1 cannot be empty." << std::endl;
+		return 1;
+	}
 	std::ifstream infile(filename.c_str());
+	if (!infile.is_open())
+	{
+		std::cerr << "Error: Could not open input file: " << filename << std::endl;
+		return 1;
+	}
 	std::ofstream outfile((filename + ".replace").c_str());
-
+	if (!outfile.is_open())
+	{
+		std::cerr << "Error: Could not create output file." << std::endl;
+		infile.close();
+		return 1;
+	}
 	while(std::getline(infile, line))
 	{
 		line = replace_line(line, s1, s2);
