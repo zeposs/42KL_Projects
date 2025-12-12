4663497510e2d8f6cdea4e1a294d2b856bfe6a77
Zep Ernest <zernest@u90z05s03.42kl.edu.my>
2025-12-12 22:05:23 +0800

    started ex00

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zernest <zernest@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/12 21:23:30 by zernest           #+#    #+#             */
+/*   Updated: 2025/12/12 21:49:01 by zernest          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+template <typename T>
+void	easyfind(T haystack, int needle)
+{
+	
+}
+
+#endif
