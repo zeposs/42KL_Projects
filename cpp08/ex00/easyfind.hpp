be44f36b0484d81e51d5321da5302463e26a9316
Zep Ernest <zernest@u82z01s05.42kl.edu.my>
2025-12-17 17:54:31 +0800

    might need a bit more work

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,17 +6,31 @@
 /*   By: zernest <zernest@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/12 21:23:30 by zernest           #+#    #+#             */
-/*   Updated: 2025/12/12 21:49:01 by zernest          ###   ########.fr       */
+/*   Updated: 2025/12/17 17:53:19 by zernest          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <exception>
+
+class NotFoundException : public std::exception {
+	public:
+		const char* what() const throw() { return ("Value not found"); }
+} ;
+
 template <typename T>
-void	easyfind(T haystack, int needle)
+typename T::iterator	easyfind(T &haystack, int needle)
 {
-	
+	typename T::iterator it = std::find(haystack.begin(), haystack.end(), needle);
+
+	if (it == haystack.end())
+		throw NotFoundException();
+	return (it);
 }
 
 #endif
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,7 +6,38 @@
 /*   By: zernest <zernest@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/12 21:23:36 by zernest           #+#    #+#             */
-/*   Updated: 2025/12/12 21:23:42 by zernest          ###   ########.fr       */
+/*   Updated: 2025/12/17 17:54:03 by zernest          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "easyfind.hpp"
+
+int main()
+{
+	std::vector<int> v;
+	v.push_back(10);
+	v.push_back(20);
+	v.push_back(30);
+	v.push_back(40);
+
+	try
+	{
+		std::cout << "Working test:\n";
+		std::vector<int>::iterator it = easyfind(v, 30);
+		std::cout << "Found: " << *it << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	try
+	{
+		std::cout << "Fail test:\n";
+		std::vector<int>::iterator it = easyfind(v, 50);
+		std::cout << "Found: " << *it << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+}
